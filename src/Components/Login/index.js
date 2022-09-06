45a86bb32873c9e7640f0b5bd050397330c9eed7
Vera Mae Piollo <veramae_piollo@cody.inc>
2022-09-06 16:28:26 +0800

    removed dummy data

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -1,115 +1,117 @@
-import React from "react";    
-import { useState, useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import { useCookies } from 'react-cookie';
+import { useCookies } from "react-cookie";
 
-import { login } from '../../Services/account.service';
+import { login } from "../../Services/account.service";
 
 import "./index.css";
-import { Container, Row, Form, Button } from "react-bootstrap"; 
-import { default as PiggyBank } from '../../img/piggy-bank.svg';
- 
-const initialValue = { 
-    emailaddress: 'vera@yahoo.com',
-    password: 'password1',
-}
+import { Container, Row, Form, Button } from "react-bootstrap";
+import { default as PiggyBank } from "../../img/piggy-bank.svg";
+
+const initialValue = {
+  emailaddress: "",
+  password: "",
+};
 
 function Login() {
- 
-    const [values, setValues] = useState(initialValue); 
-    const [cookies, setCookie] = useCookies(['token'],['user']);
-    let history = useHistory();
- 
-    useEffect(() => {
-        if (cookies.token) {
-            history.push('/home');
-        } 
-    }, [history, cookies]);
+  const [values, setValues] = useState(initialValue);
+  const [cookies, setCookie] = useCookies(["token"], ["user"]);
+  let history = useHistory();
 
-    const routeChange = (location) =>{ 
-        history.push(location); 
+  useEffect(() => {
+    if (cookies.token) {
+      history.push("/home");
     }
+  }, [history, cookies]);
 
-    const handleChange = (event) => {
-        setValues({
-          ...values,
-          [event.target.name]: event.target.value
-        })
+  const routeChange = (location) => {
+    history.push(location);
+  };
+
+  const handleChange = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleLogin = async (event) => {
+    console.log(`values: ${JSON.stringify(values)}`);
+    let response = await login({
+      body: values,
+    });
+
+    console.log(`response: ${JSON.stringify(response)}`);
+
+    if (response.error) {
+      alert(`ERROR: ${response.error}`);
+    } else {
+      setCookie("token", response.data.token, { path: "/" });
+      setCookie("user", response.data.user, { path: "/" });
+      setCookie("wallet", response.data.wallet, { path: "/" });
+
+      //goto next page
+      routeChange("/home");
     }
+  };
 
-    const handleLogin = async (event) => { 
+  return (
+    <Container>
+      <Row className="md-center">
+        <img alt="" src={PiggyBank} width="250" height="250" />
+        <br />
+        <h1 className="logolbl">PiggyOink!</h1>
+      </Row>
 
-        console.log(`values: ${JSON.stringify(values)}`);
-        let response = await login({
-            body: values
-        }); 
+      <Row>
+        <div className="loginbox">
+          <h4>Sign into your Account</h4>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control
+                type="email"
+                name="emailaddress"
+                placeholder="Email Address"
+                value={values.emailaddress}
+                onChange={handleChange}
+              />
+            </Form.Group>
 
-        console.log(`response: ${JSON.stringify(response)}`);
-
-        if(response.error){
-            alert(`ERROR: ${response.error}`);
-        }
-        else{
-            setCookie('token', response.data.token, { path: '/' });
-            setCookie('user', response.data.user, { path: '/' });
-            setCookie('wallet', response.data.wallet, { path: '/' });
-
-            //goto next page
-            routeChange("/home")
-        }
- 
-    }
-
-
-    return(
-        <Container> 
-            <Row className="md-center">
-                <img alt="" src={PiggyBank}  width="250" height="250"/> 
-                <br/> 
-                <h1 className="logolbl">PiggyOink!</h1>
-            </Row>
-
-            <Row>
-                <div className="loginbox">
-                    <h4>Sign into your Account</h4>
-                    <Form>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Control 
-                            type="email" 
-                            name="emailaddress" 
-                            placeholder="Email Address" 
-                            value={values.emailaddress}
-                            onChange={handleChange} 
-                            />
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Control 
-                            type="password" 
-                            name="password" 
-                            placeholder="Password"  
-                            value={values.password}
-                            onChange={handleChange} 
-                        />
-                    </Form.Group> 
-                    <Button 
-                        variant="primary" 
-                        type="button" 
-                        className="loginsubmit"
-                        onClick={handleLogin} 
-                    >
-                        SUBMIT
-                    </Button>
-                    </Form>
-                    <br/>  
-                    <h6>Don't have an account? <span className="registerlink" onClick={(e) => {routeChange("/register")}}>Register Now</span></h6> 
-                </div>
-            </Row>
-        </Container>
-    );
-    
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={values.password}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Button
+              variant="primary"
+              type="button"
+              className="loginsubmit"
+              onClick={handleLogin}
+            >
+              SUBMIT
+            </Button>
+          </Form>
+          <br />
+          <h6>
+            Don't have an account?{" "}
+            <span
+              className="registerlink"
+              onClick={(e) => {
+                routeChange("/register");
+              }}
+            >
+              Register Now
+            </span>
+          </h6>
+        </div>
+      </Row>
+    </Container>
+  );
 }
 
 export default Login;
- 
- 
--- src/Components/Registration/index.js
+++ src/Components/Registration/index.js
@@ -1,137 +1,135 @@
-import React from "react";    
-import { useState, useEffect } from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import { useCookies } from 'react-cookie';
+import { useCookies } from "react-cookie";
 
 import "./index.css";
-import { Container, Row, Form, Button } from "react-bootstrap"; 
-import { default as PiggyBank } from '../../img/piggy-bank.svg';
+import { Container, Row, Form, Button } from "react-bootstrap";
+import { default as PiggyBank } from "../../img/piggy-bank.svg";
 
-import { register } from '../../Services/account.service';
- 
+import { register } from "../../Services/account.service";
 
 const initialValue = {
-    firstname: 'vera',
-    lastname: 'piollo',
-    emailaddress: 'vera@yahoo.com',
-    password: 'password1',
-}
+  firstname: "",
+  lastname: "",
+  emailaddress: "",
+  password: "",
+};
 
-  
 function Login() {
+  const [values, setValues] = useState(initialValue);
+  const [cookies, setCookie] = useCookies(["token"], ["user"]);
+  let history = useHistory();
 
-    const [values, setValues] = useState(initialValue);
-    const [cookies, setCookie] = useCookies(['token'],['user']);
-    let history = useHistory();
-
-    useEffect(() => { 
-        if (cookies.token) { 
-            history.push('/home');
-        }  
-    }, [history, cookies]);
-  
-    const routeChange = (location) =>{ 
-        history.push(location); 
+  useEffect(() => {
+    if (cookies.token) {
+      history.push("/home");
     }
+  }, [history, cookies]);
 
-    const handleChange = (event) => {
-        setValues({
-          ...values,
-          [event.target.name]: event.target.value
-        })
+  const routeChange = (location) => {
+    history.push(location);
+  };
+
+  const handleChange = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSave = async (event) => {
+    let response = await register({
+      body: values,
+    });
+
+    if (response.error) {
+      alert(`ERROR: ${response.error}`);
+    } else {
+      setCookie("token", response.data.token, { path: "/" });
+      setCookie("user", response.data.user, { path: "/" });
+      setCookie("wallet", response.data.user, { path: "/" });
+
+      //goto next page
+      routeChange("/home");
     }
- 
-    const handleSave = async (event) => { 
-        let response = await register({
-            body: values
-        }); 
+  };
 
-        if(response.error){
-            alert(`ERROR: ${response.error}`);
-        }
-        else{
-            setCookie('token', response.data.token, { path: '/' });
-            setCookie('user', response.data.user, { path: '/' });
-            setCookie('wallet', response.data.user, { path: '/' });
+  return (
+    <Container>
+      <Row className="md-center">
+        <img alt="" src={PiggyBank} width="250" height="250" />
+        <br />
+        <h1 className="logolbl">PiggyOink!</h1>
+      </Row>
 
-            //goto next page
-            routeChange("/home")
-        }
- 
-    }
+      <Row>
+        <div className="registrationbox">
+          <h4>Create an Account</h4>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control
+                type="text"
+                name="firstname"
+                placeholder="First Name"
+                value={values.firstname}
+                onChange={handleChange}
+              />
+            </Form.Group>
 
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control
+                type="text"
+                name="lastname"
+                placeholder="Last Name"
+                value={values.lastname}
+                onChange={handleChange}
+              />
+            </Form.Group>
 
-    return(
-        <Container>  
-            <Row className="md-center">
-                <img alt="" src={PiggyBank}  width="250" height="250"/> 
-                <br/> 
-                <h1 className="logolbl">PiggyOink!</h1>
-            </Row>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control
+                type="text"
+                name="emailaddress"
+                placeholder="Email Address"
+                value={values.emailaddress}
+                onChange={handleChange}
+              />
+            </Form.Group>
 
-            <Row>
-                <div className="registrationbox">
-                    <h4>Create an Account</h4>
-                    <Form>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Control 
-                            type="text" 
-                            name="firstname" 
-                            placeholder="First Name" 
-                            value={values.firstname}
-                            onChange={handleChange}
-                        />
-                    </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={values.password}
+                onChange={handleChange}
+              />
+            </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Control 
-                            type="text"
-                            name="lastname" 
-                            placeholder="Last Name" 
-                            value={values.lastname}
-                            onChange={handleChange} 
-                        />
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Control 
-                            type="text" 
-                            name="emailaddress" 
-                            placeholder="Email Address"  
-                            value={values.emailaddress}
-                            onChange={handleChange} 
-                        />
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Control 
-                            type="password"
-                            name="password" 
-                            placeholder="Password"  
-                            value={values.password}
-                            onChange={handleChange} 
-                        />
-                    </Form.Group> 
-
-                    {/* onClick={(e) => {routeChange("/home")}} */}
-                    <Button 
-                        variant="primary" 
-                        type="button" 
-                        className="registersubmit" 
-                        onClick={handleSave} 
-                    >
-                        SUBMIT
-                    </Button>
-                    </Form>
-                    <br/> 
-                    <p className="backhomelink" onClick={(e) => {routeChange("/")}}>go back to Home Page</p>   
-                </div>
-            </Row>
-        </Container>
-    );
-    
+            {/* onClick={(e) => {routeChange("/home")}} */}
+            <Button
+              variant="primary"
+              type="button"
+              className="registersubmit"
+              onClick={handleSave}
+            >
+              SUBMIT
+            </Button>
+          </Form>
+          <br />
+          <p
+            className="backhomelink"
+            onClick={(e) => {
+              routeChange("/");
+            }}
+          >
+            go back to Home Page
+          </p>
+        </div>
+      </Row>
+    </Container>
+  );
 }
 
 export default Login;
- 
- 
